be5e2d94bcaaece7445bcdec8911a587cd7d8856
aacebo <44811138+aacebo@users.noreply.github.com>
2020-07-26 21:40:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var request = require('request'),
-    cors = require('cors'),
-    express = require('express');
+const request = require('request');
+const cors = require('cors');
+const express = require('express');
 
-var app = express();
+const app = express();
 
 app.get('/:module', cors(), function(req, res){
-  var url = "http://registry.npmjs.org/" + req.params.module
-  request.get({url:url, json:true}, function(err, resp, body) {
+  const url = `https://registry.npmjs.org/${req.params.module}`;
+
+  request.get({ url: url, json: true }, function(_err, _resp, body) {
     res.jsonp(body);
   });
 });
 
-app.listen(process.env.PORT || 5000);
-console.log('poop');
+app.listen(process.env.PORT || 3000);
