f329d38335d257cf33de51659db590e475ecba90
aacebo <44811138+aacebo@users.noreply.github.com>
2020-07-26 21:37:03 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var request = require('request'),
+    cors = require('cors'),
+    express = require('express');
+
+var app = express();
+
+app.get('/:module', cors(), function(req, res){
+  var url = "http://registry.npmjs.org/" + req.params.module
+  request.get({url:url, json:true}, function(err, resp, body) {
+    res.jsonp(body);
+  });
+});
+
+app.listen(process.env.PORT || 5000);
+console.log('poop');
